8f83b8b971dc5c5286eb8aed3993969f8931d7b1
Masterwill21 <william_fhernandez@soy.sena.edu.co>
2026-06-04 22:48:26 +0200

    Translate comments and output messages to Spanish

--- scripts/wireguard/listCONF.sh
+++ scripts/wireguard/listCONF.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-### Constants
+### Constantes
 
-### Funcions
+### Funciones
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
 }
@@ -10,22 +10,22 @@ err() {
 cd /etc/wireguard/configs || exit
 
 if [[ ! -s clients.txt ]]; then
-  err "::: There are no clients to list"
+  err "::: No hay clientes para listar"
   exit 1
 fi
 
-printf "\e[1m::: Clients Summary :::\e[0m\n"
+printf "\e[1m::: Resumen de Clientes :::\e[0m\n"
 
-# Present the user with a summary of the clients, fetching info from dates.
+# Muestra al usuario un resumen de los clientes, obteniendo la información de las fechas.
 {
-  echo -ne "\e[4mClient\e[0m  \t  \e[4mPublic key\e[0m  \t  "
-  echo -e "\e[4mCreation date\e[0m"
+  echo -ne "\e[4mCliente\e[0m  \t  \e[4mClave pública\e[0m  \t  "
+  echo -e "\e[4mFecha de creación\e[0m"
 
   while read -r LINE; do
     CLIENT_NAME="$(awk '{print $1}' <<< "${LINE}")"
     PUBLIC_KEY="$(awk '{print $2}' <<< "${LINE}")"
     CREATION_DATE="$(awk '{print $3}' <<< "${LINE}")"
-    # Dates are converted from UNIX time to human readable.
+    # Las fechas se convierten de tiempo UNIX a formato legible para humanos.
     CD_FORMAT="$(date -d @"${CREATION_DATE}" +'%d %b %Y, %H:%M, %Z')"
     echo -e "${CLIENT_NAME}  \t  ${PUBLIC_KEY}  \t  ${CD_FORMAT}"
   done < clients.txt
@@ -33,5 +33,5 @@ printf "\e[1m::: Clients Summary :::\e[0m\n"
 
 cd /etc/wireguard || return
 
-echo "::: Disabled clients :::"
+echo "::: Clientes deshabilitados :::"
 grep '\[disabled\] ### begin' wg0.conf | sed 's/#//g; s/begin//'
